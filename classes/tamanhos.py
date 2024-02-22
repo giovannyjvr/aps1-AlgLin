247fcaabaa403ddd1e9624f493b826ef16440297
giovannyjvr <giovannyjvr@al.insper.edu.br>
2024-02-22 17:40:22 -0300

    tirando arquivos sem uso

--- classes/tamanhos.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-# Tamanho da tela e definição do FPS
-SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-clock = pygame.time.Clock()
-FPS = 60  # Frames por Segundo
--- importes.py
+++
@@ -1,7 +0,0 @@
-import pygame
-import sys
-from classes.bola import Bola
-from classes.canhao import Canhao
-from classes.tamanhos import *
-from classes.passaro import Passaro
-import numpy as np
